c5804856a82354c633e80245472e57f86ed664fc
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-06-02 16:36:22 +0530

    Update urluploader.py

--- YuiGBot/modules/urluploader.py
+++ YuiGBot/modules/urluploader.py
@@ -83,13 +83,10 @@ ETA: {}""".format(
     
 __help__ = """
     
-• /up: reply to a direct download link to upload it to telegram as files
- 
  File To Link:
- ⦁ /transfersh: reply to a telegram file to upload it on transfersh and get direct download link
- ⦁ /tmpninja: reply to a telegram file to upload it on tmpninja and get direct download link
+ ⦁ /upload: reply to a telegram file to upload it on transfersh and get direct download link
  
  """
 
-__mod_name__ = "Uoload"    
+__mod_name__ = "Upload"    
 
