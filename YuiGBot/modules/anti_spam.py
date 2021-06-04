26c6047bbc2b0325f6e08508c90450a4d625e9b2
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-06-05 00:31:32 +0530

    #NetSHELL -p- (YUi v1.6)

---
+++ YuiGBot/modules/anti_spam.py
@@ -0,0 +1,25 @@
+__mod_name__ = "Anti-Spam 🔰" 
+
+__help__ = f"""
+
+*Admins ONLY*
+
+ • /antispam <on/off/yes/no> Will toggle our antispam tech or return your current settings.
+
+Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect 
+you and your groups by removing spam flooders as quickly as possible.
+
+*Note:* Users can appeal gbans or report spammers at @{SUPPORT_CHAT}
+
+
+*AnTi-Spam Force ON*
+
+SpamWatch
+
+t-GOD System
+
+Eagle System
+
+
+
+"""    
