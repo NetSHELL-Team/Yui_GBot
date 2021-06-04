2fb94b9a8c547ff26b417672a365af9de7f5334f
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-06-05 00:45:52 +0530

    #NetSHELL -p- (YUi v1.6)

--- YuiGBot/modules/misc.py
+++ YuiGBot/modules/misc.py
@@ -130,19 +130,6 @@ __help__ = """
 *Wikipedia:*
 
  • /wiki <query> wikipedia your query
- 
-
- • /antispam <on/off/yes/no> Will toggle our antispam tech or return your current settings
- 
-Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect 
-you and your groups by removing spam flooders as quickly as possible.
-
-*Note:* Users can appeal gbans or report spammers at @Yui_Official
-
-*AnTi-Spam Force ON*
-SpamWatch
-t-GOD System
-Eagle System
    
 
 """
