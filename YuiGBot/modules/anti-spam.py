ee84c35508f09bb45b7c3c4e4165597bf47d8869
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-06-05 00:52:08 +0530

    #NetSHELL -p- (YUi v1.6)

--- YuiGBot/modules/anti-spam.py
+++ YuiGBot/modules/anti-spam.py
@@ -1,9 +1,9 @@
 __help__ = """
 *Admins ONLY*
 
-• antispam on/off/yes/no Will toggle our antispam tech or return your current settings.
+[HOW TO USE Anti Spam](https://telegra.ph/%E1%B4%8D%CA%80%DC%94%E1%B4%B8%E1%B4%BC%E1%B5%9B%E1%B4%B1-%E1%B4%B7%E1%B5%81%E1%B4%B9%E1%B4%AC%E1%B4%BF-06-04)
 
-Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect 
+Anti Spam, used by bot devs to ban spammers across all groups. This helps protect 
 you and your groups by removing spam flooders as quickly as possible.
 
 *Note:* Users can appeal gbans or report spammers at @Yui_Official
