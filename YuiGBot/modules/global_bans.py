ef4d8248412296a54c7d855b54fe069a8f105f6e
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-08-03 17:37:37 +0530

    Update global_bans.py

--- YuiGBot/modules/global_bans.py
+++ YuiGBot/modules/global_bans.py
@@ -508,7 +508,7 @@ def gbanstat(update: Update, context: CallbackContext):
 
 
 def __stats__():
-    return f"┝ {sql.num_gbanned_users()} GBAN Users."
+    return f"╰ {sql.num_gbanned_users()} GBAN Users."
 
 
 def __user_info__(user_id):
