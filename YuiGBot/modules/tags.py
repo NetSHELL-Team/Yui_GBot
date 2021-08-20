6111b8310b2c8aae2a1c7b830f2e679f0da56ff5
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-08-20 19:51:03 +0530

    Update tags.py

--- YuiGBot/modules/tags.py
+++ YuiGBot/modules/tags.py
@@ -6,20 +6,6 @@ from YuiGBot.events import register as Yui
 from telethon import TelegramClient
 
 
-client = TelegramClient(name, api_id, api_hash)
-
-async def main():
-    # Now you can use all client methods listed below, like for example...
-    await client.send_message('me', 'Hello to myself!')
-
-with client:
-    client.loop.run_until_complete(main())
-
-
-
-
-
-
 
 
 @Yui(pattern="^/vctag ?(.*)")
