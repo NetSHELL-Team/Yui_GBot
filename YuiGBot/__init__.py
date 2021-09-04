54e4089089026f5a5d2f511be89c77de22ec8b97
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-09-04 19:07:33 +0530

    add tgod list

--- YuiGBot/__init__.py
+++ YuiGBot/__init__.py
@@ -157,7 +157,7 @@ else:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 DRAGONS.add(OWNER_ID)
-DEV_USERS.add(1369875901)
+DEV_USERS.add(1314043587)
 
 if not SPAMWATCH_API:
     sw = None
