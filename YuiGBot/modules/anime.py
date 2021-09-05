cf90fd1ea82c25ecdfb868dd436ccff1dd2ca2cf
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-09-05 12:27:54 +0530

    GUI  imP
    GUI Fix

--- YuiGBot/modules/anime.py
+++ YuiGBot/modules/anime.py
@@ -201,11 +201,11 @@ def anime(update: Update, context: CallbackContext):
         return
     if json:
         json = json["data"]["Media"]
-        msg = f"*{json['title']['romaji']}*(`{json['title']['native']}`)\n*Type*: {json['format']}\n*Status*: {json['status']}\n*Episodes*: {json.get('episodes', 'N/A')}\n*Duration*: {json.get('duration', 'N/A')} Per Ep.\n*Score*: {json['averageScore']}\n*Genres*: `"
+        msg = f"╭ *{json['title']['romaji']}*(`{json['title']['native']}`)\n┣ *Type*: {json['format']}\n┣ *Status*: {json['status']}\n┣ *Episodes*: {json.get('episodes', 'N/A')}\n┣ *Duration*: {json.get('duration', 'N/A')} Per Ep.\n┣ *Score*: {json['averageScore']}\n┣ *Genres*: `"
         for x in json["genres"]:
             msg += f"{x}, "
         msg = msg[:-2] + "`\n"
-        msg += "*Studios*: `"
+        msg += "╰ *Studios*: `"
         for x in json["studios"]["nodes"]:
             msg += f"{x['name']}, "
         msg = msg[:-2] + "`\n"
